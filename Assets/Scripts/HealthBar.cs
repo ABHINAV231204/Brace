a7876b1df35661d1fb9938f3d68c587616bf3bf5
Areeb <011235813phantom@gmail.com>
2024-06-01 22:26:09 +0530

    improved HUD and removed the separate canvas for options menu

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,11 +7,15 @@ public class HealthBar : MonoBehaviour
 {
     // Start is called before the first frame update
     public Slider slider;
+    public GameObject healthbar;
     public void setMaxHealth(int max){
         slider.maxValue = max;
     }
     public void setHealth(int value)
     {
         slider.value = value;
+    }
+    public void enable(bool val){
+        healthbar.SetActive(val);
     }
 }
