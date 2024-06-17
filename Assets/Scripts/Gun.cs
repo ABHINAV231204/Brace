7258eddb587be69d3e7fce8051a6228ee57d2744
iwdie <42923352+iwdie@users.noreply.github.com>
2024-06-17 18:31:11 +0530

    Added Interaction behavior similar to bonfire in game scene
    Player can now only use gun if he Press E   (which will make him sit near gun) then if  Press  P gun will fire

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Events;
 
 public class Gun : MonoBehaviour
 {
@@ -9,27 +10,59 @@ public class Gun : MonoBehaviour
     // Start is called before the first frame update
     public Light2D spotlight; // Assign your spotlight component in the inspector
     public float recoilforce = 0.1f;
-
+    public KeyCode interactKeySit;
+    public KeyCode interactKeyStand;
+    public bool PlayerInGunArea;
+    public UnityEvent interactActionSit;
+    public UnityEvent interactActionStand;
+    public bool characterisongun;
     
-
 
     void Start()
     {
 
     }
-
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerInGunArea = true;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        print(PlayerInGunArea);
+        if (PlayerInGunArea)
         {
-            // Create a new bullet instance
-            GameObject bulletInstance = Instantiate(bulletPrefab);
-            spotlight.intensity = Random.Range(2f, 6f);
-            transform.Translate(Vector3.right * recoilforce * Time.deltaTime);
+
+            if (Input.GetKeyDown(interactKeySit))
+            {
+                interactActionSit.Invoke();
+                characterisongun = true;
+               
+            }
+
+            if (Input.GetKeyDown(interactKeyStand))
+            {
+                interactActionStand.Invoke();
+                characterisongun = false;
+            }
+
+            if(characterisongun) {
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    // Create a new bullet instance
+                    GameObject bulletInstance = Instantiate(bulletPrefab);
+                    spotlight.intensity = Random.Range(2f, 6f);
+                    transform.Translate(Vector3.right * recoilforce * Time.deltaTime);
+
+
+
+                }
+            }
             
-
-
         }
+            
     }
 }
