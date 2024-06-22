2991d44ccde45f4b25dc149e1a3dfee039a0962a
Areeb <011235813phantom@gmail.com>
2024-06-22 20:04:38 +0530

    bugs fixed for the menu

--- Assets/Scripts/SettingsOptions.cs
+++ Assets/Scripts/SettingsOptions.cs
@@ -18,13 +18,29 @@ public class SettingsOptions : MonoBehaviour
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
-         for(int i=0;i<resolutions.Length;i++)
-            {
-                string option = resolutions[i].width +" x " + resolutions[i].height;
-                options.Add(option);
-            }
-        resolutionDropdown.AddOptions(options);
+        int currentResolutionIndex = 0;
 
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetVolume (float volume)
